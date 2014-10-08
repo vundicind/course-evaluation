b1703cc8f36204b14396ba60240bf8ac8096e69d
vundicind <vundicind@gmail.com>
2014-10-08 15:33:01 +0300

    Create model Course

---
+++ backend/models/Course.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "course".
+ *
+ * @property integer $id
+ * @property string $name
+ */
+class Course extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'course';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['name'], 'string', 'max' => 50]
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'name' => Yii::t('app', 'Course Name'),
+        ];
+    }
+}
