ca4bfd7dcab9c777de29c72494563089b5eafced
vundicind <vundicind@gmail.com>
2014-12-16 18:15:59 +0200

    Fix some typos

--- backend/views/specialty/_form.php
+++ backend/views/specialty/_form.php
@@ -2,6 +2,9 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use app\models\Faculty;
+use app\models\StudyCycle;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Specialty */
@@ -14,9 +17,9 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => 255]) ?>
 
-    <?= $form->field($model, 'faculty_id')->textInput() ?>
+    <?= $form->field($model, 'faculty_id')->dropDownList(ArrayHelper::map(Faculty::find()->asArray()->all(), 'id', 'name')) ?>
 
-    <?= $form->field($model, 'study_cycle_id')->textInput() ?>
+    <?= $form->field($model, 'study_cycle_id')->dropDownList(ArrayHelper::map(StudyCycle::find()->asArray()->all(), 'id', 'name')) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
