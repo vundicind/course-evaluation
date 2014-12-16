26fb189e58a7975c23ce99510825347daa223525
vundicind <vundicind@gmail.com>
2014-12-16 17:22:14 +0200

    Add migration for StudyCycle entity

---
+++ console/migrations/m141216_151416_create_study_cycle.php
@@ -0,0 +1,28 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m141216_151416_create_study_cycle extends Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+        
+        $this->createTable('{{%study_cycle}}', [
+            'id' => Schema::TYPE_PK,
+            'name' => Schema::TYPE_STRING . ' NOT NULL',
+        ], $tableOptions);
+        
+        $this->createIndex('NAME', '{{%study_cycle}}', 'name', true);
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%study_cycle}}');
+    }
+}
