72d4421cccb13137b1aec88e517c4827804042b8
vundicind <vundicind@gmail.com>
2014-12-16 17:08:47 +0200

    Add Faculty entity

---
+++ backend/models/Faculty.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "faculty".
+ *
+ * @property integer $id
+ * @property string $name
+ */
+class Faculty extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'faculty';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['name'], 'string', 'max' => 50],
+            [['name'], 'unique'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'name' => Yii::t('app', 'Name'),
+        ];
+    }
+}
