5b8a0970b8e939841ae1649fd86e189f239deee1
vundicind <vundicind@gmail.com>
2014-12-18 12:07:32 +0200

    Add migration for Course entity

---
+++ console/migrations/m141218_100550_create_course_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use yii\db\Schema;
+use yii\db\Migration;
+
+class m141218_100550_create_course_table extends Migration
+{
+    public function up()
+    {
+        $tableOptions = null;
+        if ($this->db->driverName === 'mysql') {
+            // http://stackoverflow.com/questions/766809/whats-the-difference-between-utf8-general-ci-and-utf8-unicode-ci
+            $tableOptions = 'CHARACTER SET utf8 COLLATE utf8_unicode_ci ENGINE=InnoDB';
+        }
+        
+        $this->createTable('{{%course}}', [
+            'id' => Schema::TYPE_PK,
+            'name' => Schema::TYPE_STRING . ' NOT NULL',
+        ], $tableOptions);
+    }
+
+    public function down()
+    {
+        $this->dropTable('{{%course}}');
+    }
+}
