ff8060f4141aff102f95a6883fd13b43c489bf26
vundicind <vundicind@gmail.com>
2015-01-08 13:21:51 +0200

    Fix a typo

--- backend/views/group/_form.php
+++ backend/views/group/_form.php
@@ -16,7 +16,7 @@ use app\models\Specialty;
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => 50]) ?>
     
-    <?= $form->field($model, 'specialty_id')->dropDownList(ArrayHelper::map(Specialty::find()->asArray()->all(), 'id', 'name')) ?>
+    <?= $form->field($model, 'specialty_id')->dropDownList(ArrayHelper::map(Specialty::find()->asArray()->all(), 'id', 'name'), ['prompt' => Yii::t('app', 'Select Specialty')]) ?>
 
     <div class="form-group">
         <?= Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
