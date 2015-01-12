207930415d102657aa3f8f9bee810ca7f3b69d99
vundicind <vundicind@gmail.com>
2015-01-12 15:04:57 +0200

    Misc

--- frontend/controllers/SurveyController.php
+++ frontend/controllers/SurveyController.php
@@ -3,11 +3,44 @@
 namespace frontend\controllers;
 
 use common\models\Faculty;
+use common\models\StudyCycle;
+use common\models\StudyForm;
+use common\models\Specialty;
+use common\models\Group;
 
 class SurveyController extends \yii\web\Controller
 {
     public function actionIndex()
     {
+    	if(!empty($_GET['f']) && empty($_GET['sc']))
+    	{
+    		$studyCycles = StudyCycle::find()->all();
+    		return $this->render('index', ['studyCycles' => $studyCycles, 'f' => $_GET['f']]);
+    	}
+
+    	if(!empty($_GET['sc']) && empty($_GET['sf']))
+    	{
+    		$studyForms = StudyForm::find()->all();
+    		return $this->render('index', ['studyForms' => $studyForms, 'f' => $_GET['f'], 'sc' => $_GET['sc']]);
+    	}
+    	
+    	if(!empty($_GET['sf']))
+    	{
+    		$specialties = Specialty::find()
+    			->andFilterWhere(['faculty_id' => $_GET['f']])
+    			->andFilterWhere(['study_cycle_id' => $_GET['sc']])
+    			->all();
+    		return $this->render('index', ['specialties' => $specialties]);
+    	}
+
+    	if(!empty($_GET['s']))
+    	{
+    		$groups = Group::find()
+    		->andFilterWhere(['specialty_id' => $_GET['s']])
+    		->all();
+    		return $this->render('index', ['groups' => $groups]);
+    	}
+    	 
         $faculties = Faculty::find()->all();
         return $this->render('index', ['faculties' => $faculties]);
     }
