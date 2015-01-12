4b20b7643a88c9f7dd900d6e00148fdaba5ddd2f
vundicind <vundicind@gmail.com>
2015-01-12 14:54:53 +0200

    Move to Common context

--- backend/controllers/StudyFormController.php
+++ backend/controllers/StudyFormController.php
@@ -3,7 +3,7 @@
 namespace backend\controllers;
 
 use Yii;
-use app\models\StudyForm;
+use common\models\StudyForm;
 use yii\data\ActiveDataProvider;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
