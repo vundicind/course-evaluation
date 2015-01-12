8dcd9eb32b8123c3c309ff504833df36e752a8df
vundicind <vundicind@gmail.com>
2015-01-12 13:47:29 +0200

    Move model to Common context

--- backend/controllers/FacultyController.php
+++ backend/controllers/FacultyController.php
@@ -3,7 +3,7 @@
 namespace backend\controllers;
 
 use Yii;
-use app\models\Faculty;
+use common\models\Faculty;
 use yii\data\ActiveDataProvider;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
---
+++ common/models/Faculty.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "faculty".
+ *
+ * @property integer $id
+ * @property string $name
+ * @property string short_name
+ */
+class Faculty extends \yii\db\ActiveRecord
+{
+    /**
+     * @inheritdoc
+     */
+    public static function tableName()
+    {
+        return 'faculty';
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function rules()
+    {
+        return [
+            [['name'], 'required'],
+            [['name', 'short_name'], 'string', 'max' => 255],
+            [['name', 'short_name'], 'unique'],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => Yii::t('app', 'ID'),
+            'name' => Yii::t('app', 'Name'),
+            'short_name' => Yii::t('app', 'Short Name'),
+        ];
+    }
+}
