947cb3550baff97cee8da3b9544f2880aa9fc783
vundicind <vundicind@gmail.com>
2015-01-13 15:07:45 +0200

    Modify layout

--- backend/views/group-activity/_form.php
+++ backend/views/group-activity/_form.php
@@ -20,11 +20,11 @@ use common\models\Semester;
 
     <?= $form->field($model, 'group_id')->dropDownList(ArrayHelper::map(Group::find()->asArray()->all(), 'id', 'name'), ['prompt' => Yii::t('app', 'Select Group')]) ?>
 
-    <?= $form->field($model, 'activity_type_id')->dropDownList(ArrayHelper::map(ActivityType::find()->asArray()->all(), 'id', 'name'), ['prompt' => Yii::t('app', 'Select Activity Type')]) ?>
-
     <?= $form->field($model, 'course_id')->dropDownList(ArrayHelper::map(Course::find()->asArray()->all(), 'id', 'name'), ['prompt' => Yii::t('app', 'Select Course')]) ?>
 
     <?= $form->field($model, 'instructor_id')->dropDownList(ArrayHelper::map(Instructor::find()->asArray()->all(), 'id', function ($element) { return $element['first_name'] . ' ' . $element['last_name'];}), ['prompt' => Yii::t('app', 'Select Instructor')]) ?>
+    
+    <?= $form->field($model, 'activity_type_id')->dropDownList(ArrayHelper::map(ActivityType::find()->asArray()->all(), 'id', 'name'), ['prompt' => Yii::t('app', 'Select Activity Type')]) ?>
     
     <?= $form->field($model, 'semester_id')->dropDownList(ArrayHelper::map(Semester::find()->asArray()->all(), 'id', 'name'), ['prompt' => Yii::t('app', 'Select Semester')]) ?>
     
