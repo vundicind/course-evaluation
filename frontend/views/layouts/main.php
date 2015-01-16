c3182c59d7872c2f0d6be6d4d740ca6bc4b44e6f
vundicind <vundicind@gmail.com>
2015-01-16 20:46:53 +0200

    No need to login in frontend

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -37,15 +37,6 @@ AppAsset::register($this);
                 ['label' => 'About', 'url' => ['/site/about']],
                 ['label' => 'Contact', 'url' => ['/site/contact']],
             ];
-            if (Yii::$app->user->isGuest) {
-                $menuItems[] = ['label' => 'Login', 'url' => ['/site/login']];
-            } else {
-                $menuItems[] = [
-                    'label' => 'Logout (' . Yii::$app->user->identity->username . ')',
-                    'url' => ['/site/logout'],
-                    'linkOptions' => ['data-method' => 'post']
-                ];
-            }
             echo Nav::widget([
                 'options' => ['class' => 'navbar-nav navbar-right'],
                 'items' => $menuItems,
